c1637875d3f65399764db44db084ae79377b58a1
Tiago "KEBAB" Guerreiro <mewseven7777777@gmail.com>
2018-11-10 18:41:17 +0000

    BinarySearchTree added

--- src/dataStructures/EmptyDictionaryException.java
+++ src/dataStructures/EmptyDictionaryException.java
@@ -1,4 +1,6 @@
 package dataStructures;
 
-public class EmptyDictionaryException extends RuntimeException {
+public class EmptyDictionaryException extends RuntimeException{
+    static final long serialVersionUID = 0L;
 }
+
--- src/dataStructures/OrderedDictionary.java
+++ src/dataStructures/OrderedDictionary.java
@@ -1,17 +1,17 @@
-package dataStructures;
+package dataStructures;  
 
 /**
  * Ordered Dictionary interface
  *
  * @author AED team
  * @version 1.0
- *
+ * 
  * @param <K> Generic type Key, must extend comparable
- * @param <V> Generic type Value
+ * @param <V> Generic type Value 
  */
-public interface OrderedDictionary<K extends Comparable<K>, V>
-        extends Dictionary<K,V>
-{
+public interface OrderedDictionary<K extends Comparable<K>, V> 
+    extends Dictionary<K,V>
+{                                                                   
 
     /**
      * Returns the entry with the smallest key in the dictionary.
@@ -30,12 +30,12 @@ public interface OrderedDictionary<K extends Comparable<K>, V>
     Entry<K,V> maxEntry( ) throws EmptyDictionaryException;
 
     /* (non-Javadoc)
-     * Returns an iterator of the entries in the dictionary
+     * Returns an iterator of the entries in the dictionary 
      * which preserves the key order relation.
-     *
+     * 
      * @see dataStructures.Dictionary#iterator()
      */
-    // Iterator<Entry<K,V>> iterator( );
+    // Iterator<Entry<K,V>> iterator( );  
 
-}
+} 
 
